d53fa4227c2b2c8be25eefa4f19905ac653c7d93
Mira Anand <miraanand@comcast.net>
2024-09-25 14:28:15 -0700

    2nd commit

--- Lab14MiraAnand.cpp
+++ Lab14MiraAnand.cpp
@@ -2,7 +2,26 @@
 // Module 5, Lesson: OOP Introduction/Review, Assignment: Color Class
 // Coded using Visual Studio Code for Mac
 
+#include <iostream>
+using namespace std;
+
+// creation of Color class
+class Color
+{
+    // private member variables
+    private:
+        int red;
+        int green;
+        int blue;
+    // public member functions
+    // using inline functions for setters and getters, since functions are 1 line
+    public:
+        // creating all setters together, to keep it organized
+        void setRed(int r)  { red = r; }
+        void setGreen(int g) { green = g; }
+};
+
 int main()
 {
-    
+
 }
