dd6f464787e0a32c8cb3bde8f1e79772702459e9
Mira Anand <miraanand@comcast.net>
2024-09-25 15:03:41 -0700

    5th commit

--- Lab14MiraAnand.cpp
+++ Lab14MiraAnand.cpp
@@ -33,6 +33,7 @@ class Color
             cout << "Red value: " << red << ", ";
             cout << "Green value: " << green << ", ";
             cout << "Blue value: " << blue << endl;
+            cout << endl;
         }
 };
 
@@ -41,7 +42,39 @@ int main()
     // creation of 1st color object
     Color color1;
     // using setters to populate the object with data
+    // RGB color values are within the range of 0-255, so data has to be within that range
     color1.setRed(255);
+    color1.setGreen(0);
+    color1.setBlue(0);
+    // outputting color values to the console using the object's print() method
+    color1.print();
+
+    // creation of 2nd color object
+    Color color2;
+    // using setters to populate the object with data
+    color2.setRed(0);
+    color2.setGreen(255);
+    color2.setBlue(0);
+    // outputting color values to the console using the object's print() method
+    color2.print();
+
+    // creation of 3rd color object
+    Color color3;
+    // using setters to populate the object with data
+    color3.setRed(0);
+    color3.setGreen(0);
+    color3.setBlue(255);
+    // outputting color values to the console using the object's print() method
+    color3.print();
+
+    // creation of 4th color object
+    Color color4;
+    // using setters to populate the object with data
+    color4.setRed(255);
+    color4.setGreen(255);
+    color4.setBlue(0);
+    // outputting color values to the console using the object's print() method
+    color4.print();
 
     return 0;
 }
