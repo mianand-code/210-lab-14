282440119d2dc92c17c2052e5e8a19699a898875
Mira Anand <miraanand@comcast.net>
2024-09-26 12:22:38 -0700

    7th commit - took a break, coming back to code 1 day later. Revising to include random # generator, adding detailed comments

--- Lab14MiraAnand.cpp
+++ Lab14MiraAnand.cpp
@@ -2,8 +2,15 @@
 // Module 5, Lesson: OOP Introduction/Review, Assignment: Color Class
 // Coded using Visual Studio Code for Mac
 
+#include <cstdlib> // needed to generate a random number
+#include <ctime> // needed to generate a random number
 #include <iostream>
 using namespace std;
+
+// declaration and initialization of a const int variable to represent the maximum RGB hue value
+// creation of this variable will help to generate a random number for each RGB hue value in main()
+// initializing the variable with the value 256 because rand() % 256 = 0-255, which is the range of possible values for each hue according to the RGB color model
+const int MAX_HUE_VALUE = 256;
 
 // creation of Color class
 class Color
@@ -17,10 +24,18 @@ class Color
     // using inline functions for setters and getters, since functions are 1 line
     public:
         // creating all setters together, to keep it organized
+        // void setRed(int r), void setGreen(int g), void setBlue(int b) function header
+        // DESCRIPTION: these functions will "set" the RGB hue values for each Color object
+        // ARGUMENTS: int r, int g, & int b. Represents the hue value for each color
+        // RETURNS: nothing, void functions
         void setRed(int r)      { red = r; }
         void setGreen(int g)    { green = g; }
         void setBlue(int b)     { blue = b; }
         // creating all getters together, to keep it organized
+        // int getRed() const, int getGreen() const, int getBlue() const function header
+        // DESCRIPTION: these functions will "get" the RGB hue values for each Color object after they are "set"
+        // ARGUMENTS: no arguments/parameters
+        // RETURNS: the int hue value for red, green, and blue
         // adding a trailing const to each, since getters do not change an object's data
         int getRed() const      { return red; }
         int getGreen() const    { return green; }
@@ -29,7 +44,7 @@ class Color
         // creating a member print() method to print the object's data
         void print()
         {
-            cout << "Color - RGB color values:" << endl;
+            cout << "Color - RGB hue values:" << endl;
             cout << "Red value: " << red << ", ";
             cout << "Green value: " << green << ", ";
             cout << "Blue value: " << blue << endl;
@@ -39,54 +54,57 @@ class Color
 
 int main()
 {
-    // creation of 1st color object
+    srand(time(0)); // needed as the first line in main() to generate random number
+
+    // creating each Color object separately in order to gain practice with creating various objects and with using the dot operator to access public member functions
+    
+    // creation of 1st Color object
     Color color1;
     // using setters to populate the object with data
-    // RGB color values are within the range of 0-255, so data has to be within that range
-    // 255, 0, 0 are the RGB values that create the color red
-    color1.setRed(255);
-    color1.setGreen(0);
-    color1.setBlue(0);
+    // populating the object with random number data, rand() % MAX_HUE_VALUE = 0-255
+    color1.setRed(rand() % MAX_HUE_VALUE);
+    color1.setGreen(rand() % MAX_HUE_VALUE);
+    color1.setBlue(rand() % MAX_HUE_VALUE);
     // outputting color values to the console using the object's print() method
     color1.print();
 
-    // creation of 2nd color object
+    // creation of 2nd Color object
     Color color2;
     // using setters to populate the object with data
-    // 0, 255, 0 are the RGB values that create the color green
-    color2.setRed(0);
-    color2.setGreen(255);
-    color2.setBlue(0);
+    // populating the object with random number data, rand() % MAX_HUE_VALUE = 0-255
+    color2.setRed(rand() % MAX_HUE_VALUE);
+    color2.setGreen(rand() % MAX_HUE_VALUE);
+    color2.setBlue(rand() % MAX_HUE_VALUE);
     // outputting color values to the console using the object's print() method
     color2.print();
 
-    // creation of 3rd color object
+    // creation of 3rd Color object
     Color color3;
     // using setters to populate the object with data
-    // 0, 0, 255 are the RGB values that create the color blue
-    color3.setRed(0);
-    color3.setGreen(0);
-    color3.setBlue(255);
+    // populating the object with random number data, rand() % MAX_HUE_VALUE = 0-255
+    color3.setRed(rand() % MAX_HUE_VALUE);
+    color3.setGreen(rand() % MAX_HUE_VALUE);
+    color3.setBlue(rand() % MAX_HUE_VALUE);
     // outputting color values to the console using the object's print() method
     color3.print();
 
-    // creation of 4th color object
+    // creation of 4th Color object
     Color color4;
     // using setters to populate the object with data
-    // 255, 255, 0 are the RGB values that create the color yellow
-    color4.setRed(255);
-    color4.setGreen(255);
-    color4.setBlue(0);
+    // populating the object with random number data, rand() % MAX_HUE_VALUE = 0-255
+    color4.setRed(rand() % MAX_HUE_VALUE);
+    color4.setGreen(rand() % MAX_HUE_VALUE);
+    color4.setBlue(rand() % MAX_HUE_VALUE);
     // outputting color values to the console using the object's print() method
     color4.print();
 
-    // creation of 5th color object
+    // creation of 5th Color object
     Color color5;
     // using setters to populate the object with data
-    // 255, 192, 203 are the RGB values that create the color pink
-    color5.setRed(255);
-    color5.setGreen(192);
-    color5.setBlue(203);
+    // populating the object with random number data, rand() % MAX_HUE_VALUE = 0-255
+    color5.setRed(rand() % MAX_HUE_VALUE);
+    color5.setGreen(rand() % MAX_HUE_VALUE);
+    color5.setBlue(rand() % MAX_HUE_VALUE);
     // outputting color values to the console using the object's print() method
     color5.print();
 
